95c0b4fb8ec8c87350b91b6fae4086771ca0be06
Nidhog-tm <>
2019-08-05 21:06:53 +0000

    add get_db_list

---
+++ get_db_list.py
@@ -0,0 +1,16 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table    = dynamodb.Table('MT_serveres_test')
+
+def get_person(id):
+    response = table.get_item(
+            Key={
+                 'test_id': id
+            }
+        )
+    return response['Item']
+
+def handler(event, context):
+    person = get_person('001')
+    return person
