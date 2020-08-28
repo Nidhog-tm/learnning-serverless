7ad4e26d86e413c3af0bd969d19f906c55a97120
Nidhog-tm <nidhog.1031@gmail.com>
2020-08-28 17:31:07 +0900

    swagger.yaml 追加

--- app/get_db_list.py
+++ app/get_db_list.py
@@ -15,7 +15,9 @@ def handler(event, context):
     return {
         'statusCode': 200,
         'headers': {
-            "Access-Control-Allow-Origin": "*"
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            'Access-Control-Allow-Methods': 'OPTIONS,POST',
         },
         'body': json.dumps({
             'result': person
